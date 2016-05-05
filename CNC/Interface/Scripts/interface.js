36fa582511b73d7da58f2fc19b5ba79c8546069f
brn91 <terewan23@gmail.com>
2016-05-06 01:41:47 +0200

    All functions of Milestone2 implemented

--- CNC/Interface/Scripts/interface.js
+++ CNC/Interface/Scripts/interface.js
@@ -1,7 +1,13 @@
 getStatus();
-var myVar = setInterval(getStatus, 60000);
+getTasks();
+var myStatus = setInterval(getStatus, 10000);
+var myTasks = setInterval(getTasks, 60000);
 
-//Gets data for #Statustable
+
+
+/*----- Status -----*/
+
+//Gets data for #statusTable
 function getStatus() {
   var xhr = new XMLHttpRequest();
   
@@ -20,57 +26,54 @@ function getStatus() {
   xhr.send(null);
 }
 
-//Update #Statustable with aquired data
+//Update #statusTable with aquired data
 function updateStatusTable(data) {
   var output = "";
 
   for(var i = 0; i < data.length; i++) {
       output += "<tr><td>" +
-      data[i].workload +
-      "</td><td>" +
-      data[i].ip +
-      "</td><td>" +
-      data[i].task + " (" + data[i].id + ")" +
-      "</td><td>" + 
-      '<button onclick="toggleStatusButton(this.id)" type="button" class="Statusbutton" id="'+ data[i].id + 'stop" >Stop</button>' + "</td>" +
-      "</td></tr>";
+      data[i].id + "</td><td>" +
+      data[i].ip + "</td><td>" +
+      data[i].task + "</td><td>" +
+      data[i].workload + "</td><td>";
+      var buttonId = data[i].id + " button";
+      if(data[i].workload === 0){ 
+        output += '<button onclick="sendStatusButtonToggle(this.id, true)" type="button" class="tableButtonGreen" id="' + buttonId + '">start</button>' + "</td>" +
+        "</td></tr>";
+      }else{
+        output += '<button onclick="sendStatusButtonToggle(this.id, false)" type="button" class="tableButtonRed" id="' + buttonId + '">stop</button>' + "</td>" +
+        "</td></tr>";
+      }
 
-      document.getElementById("statusBody").innerHTML = output; 
+      document.getElementById("statusBody").innerHTML = output;    
   }
   
   //Adjust height of #Status section 
-  var tableHeight = document.getElementById("Statustable").scrollHeight;
-  document.getElementById("Status").style.height = tableHeight + "px";
+  var tableHeight = document.getElementById("statusTable").scrollHeight;
+  document.getElementById("Status").style.height = (tableHeight + 100) + "px";
 }
 
-//StatusButton toggle
-function toggleStatusButton(id){
-    if(document.getElementById(id).innerHTML == "Start"){
-      document.getElementById(id).style.backgroundColor =  "#4CAF50";
-      sendStatusButtonToggle(id);
-      document.getElementById(id).innerHTML = "Stop";
-    }else if(document.getElementById(id).innerHTML == "Stop"){
-      document.getElementById(id).style.backgroundColor =  "#f44336";
-      sendStatusButtonToggle(id);
-      document.getElementById(id).innerHTML = "Start";
-    }
-}
-
-//StatusButton /api/Status POST Integration
-function sendStatusButtonToggle(buttonId){
+//StatusButton /api/Status POST integration (send START/STOP)
+function sendStatusButtonToggle(buttonId, buttonStatus){
   var xhr = new XMLHttpRequest();
 
   xhr.open('POST', 'http://botnet.artificial.engineering:8080/api/Status/');
-  xhr.responseType = 'plain';
+  xhr.responseType = 'json';
   xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+  xhr.setRequestHeader('Token', 'WAW-RohBots');
   
   var data = {
-    id: buttonId.charAt(0),
-    action: document.getElementById(buttonId).innerHTML
+    "id": parseInt(buttonId.substr(0,buttonId.indexOf(' '))),
+    "status": buttonStatus
     };
-  
+   
   xhr.send(JSON.stringify(data));
+  setTimeout(getStatus, 1000);
 }
+
+
+
+/*----- Tasks -----*/
 
 //POST task formular
 function submitTaskForm(form) {    
@@ -78,17 +81,58 @@ function submitTaskForm(form) {
 
   xhr.open('POST', 'http://botnet.artificial.engineering:8080/api/Tasks/');
   xhr.responseType = 'json';
-  xhr.setRequestHeader('Content-type', 'text/plain; charset=UTF-8');
+  xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+  xhr.setRequestHeader('Token', 'WAW-RohBots');
   
   var data = {
-    id: form.id.value,
     type: form.type.value,
     data: {
       input: form.data.value,
-      output: null
     }
   };
   
   xhr.send(JSON.stringify(data));
+  setTimeout(getTasks, 1000);
 }
 
+//Gets data for #taskTable
+function getTasks() {
+  var xhr = new XMLHttpRequest();
+  
+  xhr.open('GET', 'http://botnet.artificial.engineering:8080/api/Tasks');
+  xhr.responseType = 'json';
+
+  xhr.onload = function() {
+
+      var data = xhr.response;
+      if (data !== null) {
+          updateTaskTable(data); // Parsed JSON object
+      }
+
+  };
+
+  xhr.send(null);
+}
+
+//Update #taskTable with aquired data
+function updateTaskTable(data){
+  var output = "";
+
+  for(var i = 0; i < data.length; i++) {
+    output += "<tr><td>" +
+    data[i].id +
+    "</td><td>" +
+    data[i].type +
+    "</td><td>" +
+    data[i].data.input +
+    "</td><td>" + 
+    data[i].data.output +
+    "</td></tr>";
+
+    document.getElementById("tasksBody").innerHTML = output; 
+  }
+
+  //Adjust height of #Task section 
+  var tableHeight = document.getElementById("tasksTable").scrollHeight;
+  document.getElementById("Tasks").style.height = (tableHeight + 100) +"px";
+}
